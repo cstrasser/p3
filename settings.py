44b8d843c3fbf3d269d4584104a1df47384a7e37
Chris Strasser <Strasser@BRK-ITLT11-001.local>
2015-04-27 14:19:11 -0400

    testing git

--- settings.py
+++ settings.py
@@ -81,12 +81,6 @@ DATABASES = {
 
 
 
- # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
